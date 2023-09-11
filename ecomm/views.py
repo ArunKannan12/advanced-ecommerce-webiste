b4a17489ffdb387f65a68e6421acc6ed41388d4c
Arunkannan12 <arunkannan1701@gmail.com>
2023-09-11 12:10:18 +0530

    rating added to home page

--- ecomm/views.py
+++ ecomm/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render,get_object_or_404
-from store.models import Product
+from store.models import Product,ReviewRating
 
-def home(request):
-    product=Product.objects.all().filter(is_available=True)
+def home(request):    
+    product=Product.objects.all().filter(is_available=True).order_by('-created_date')
+    for i in product:
+
+        reviews=ReviewRating.objects.filter(product_id=i.id,status=True)
     context={
-        'product':product
+        'product':product,
+        'reviews':reviews
     }
     return render(request,'store/home.html',context)
